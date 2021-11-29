f36d527afcd656189076b5f1e077f3684d908e1b
JWKO-EB <jwko@everybot.net>
2021-11-29 09:08:28 +0900

    create 메소드 생성

--- src/main/java/com/example/sbaws/service/TodoService.java
+++ src/main/java/com/example/sbaws/service/TodoService.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class TodoService {
@@ -17,5 +19,22 @@ public class TodoService {
         repository.save(entity);
         TodoEntity savedEntity = repository.findById(entity.getId()).get();
         return savedEntity.getTitle();
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity) {
+        if (entity == null) {
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null");
+        }
+
+        if (entity.getUserId() == null) {
+            log.warn("Unknown user.");
+            throw new RuntimeException("Unknown user.");
+        }
+
+        repository.save(entity);
+        log.info("Entity Id: {} is saved", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
     }
 }
