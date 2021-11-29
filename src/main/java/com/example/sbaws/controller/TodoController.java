bbdd90805d9409cfc6212037874ee239c99f1956
jaewoong <jaewoong.go@gmail.com>
2021-11-29 23:45:51 +0900

    createTodo 구현
    Entity, DTO 변환이 한번에 잘 그려지지 않고, stream()등 자바에 특화된 기능이 낮설다.

--- src/main/java/com/example/sbaws/controller/TodoController.java
+++ src/main/java/com/example/sbaws/controller/TodoController.java
@@ -1,15 +1,17 @@
 package com.example.sbaws.controller;
 
 import com.example.sbaws.dto.ResponseDTO;
+import com.example.sbaws.dto.TodoDTO;
+import com.example.sbaws.model.TodoEntity;
 import com.example.sbaws.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
@@ -25,6 +27,25 @@ public class TodoController {
 
         ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
         return ResponseEntity.ok().body(response);
+    }
 
+    @PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+        try {
+            String temporaryUserId = "temporary-user";
+            TodoEntity entity = TodoDTO.toEntity(dto);
+            entity.setId(null);
+            entity.setUserId(temporaryUserId);
+            List<TodoEntity> entities = service.create(entity);
+
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 }
