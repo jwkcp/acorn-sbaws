c0bfbbe131a1d53c8b431a22f1ae2e47b6bf91bd
jwkcp <jaewoong.go@gmail.com>
2021-11-30 09:01:05 +0900

    retrieve, update 작성 중

--- src/main/java/com/example/sbaws/controller/TodoController.java
+++ src/main/java/com/example/sbaws/controller/TodoController.java
@@ -48,4 +48,33 @@ public class TodoController {
             return ResponseEntity.badRequest().body(response);
         }
     }
+
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList() {
+        try {
+            String temporaryUserId = "temporary-user";
+
+            List<TodoEntity> entities = service.retrieve(temporaryUserId);
+
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto) {
+        String temporaryUserId = "temporary-user";
+
+        TodoEntity entity = TodoDTO.toEntity(dto);
+        entity.setUserId(temporaryUserId);
+        List<TodoEntity> entities = service.update(entity);
+
+        // Start to here
+    }
 }
