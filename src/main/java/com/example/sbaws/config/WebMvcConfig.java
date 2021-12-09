3174a3e44ab25174c3aaa4eaa762605f12ff9725
jwkcp <jaewoong.go@gmail.com>
2021-12-10 08:59:38 +0900

    cors 설정 중

---
+++ src/main/java/com/example/sbaws/config/WebMvcConfig.java
@@ -0,0 +1,9 @@
+package com.example.sbaws.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final long MAX_AGE_SECS = 3600;
+}
