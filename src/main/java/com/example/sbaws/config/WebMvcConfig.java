6859bcbfd2531170acbd412d8d7671d902985e37
jaewoong <jaewoong.go@gmail.com>
2021-12-19 23:13:51 +0900

    03장 완료

--- src/main/java/com/example/sbaws/config/WebMvcConfig.java
+++ src/main/java/com/example/sbaws/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    // 향후 로컬 변수로 설정 예정
     private final long MAX_AGE_SECS = 3600;
 
     @Override
