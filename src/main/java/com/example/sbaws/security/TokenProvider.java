af4e4c4197001fab762e0587acf24e3e5f648353
jaewoong <jaewoong.go@gmail.com>
2021-12-25 18:57:30 +0900

    security에 token 생성 및 검증 로직 추가

---
+++ src/main/java/com/example/sbaws/security/TokenProvider.java
@@ -0,0 +1,51 @@
+package com.example.sbaws.security;
+
+import com.example.sbaws.model.UserEntity;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public class TokenProvider {
+    private static final String SECRET_KEY = "NMA8JPCTFuna59f5";
+
+    public String create(UserEntity userEntity) {
+        Date expiryDate = Date.from(
+                Instant.now().plus(1, ChronoUnit.DAYS));
+
+        /*
+        { // header
+            "alg": "H512"
+        }.
+        { // payload
+            "sub": "40288093784915d201784916a40c0001",
+            "iss": "demo app",
+            "iat": 1595733657,
+            "exp": 1596597657
+        }.
+        // SECRET_KEY를 이용해 서명한 부분분
+        Nn4d1MOVLZg79sfFACTIpCPKqWmpZMZQsbNrXdJJNWkRv50_l7bPLQPwhMobT4vBOG6Q3JYjhDrKFlBSaUxZOg
+        */
+
+        return Jwts.builder()
+                // header에 들어갈 내용 및 서명을 하기 위한 SECRET_KEY
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .setSubject(userEntity.getId())
+                .setIssuer("demo app")
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .compact();
+    }
+
+    public String validateAndGetUserId(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+}
